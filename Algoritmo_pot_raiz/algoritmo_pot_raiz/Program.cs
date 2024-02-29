e477d778e5feb8955d1ad0946c3999174263cc26
SPFENIX <cgofenix@hotmail.com>
2024-02-28 21:59:38 -0500

    Calculo de la  distancia entre dos puntos.

---
+++ Algoritmo_pot_raiz/algoritmo_pot_raiz/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Realizar un algoritmo que me pida tres numeros por pantalla, con el primer numero hallar el cuadrado, con el segundo hallar
+        //la raiz cudrada y con el tercero hallar la raiz cubica. Mostrar en pantalla los tres resultados en pantalla.
+        int num1,num2,num3;
+        double cuad, raiz, racub;
+        Console.WriteLine("Numeros positivos");
+        Console.WriteLine("Digite el primer numero");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nDigite el segundo numero");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nDigite el tercer numero");
+        num3 = Convert.ToInt32(Console.ReadLine());
+
+        cuad = num1 * num1;
+        raiz = Math.Sqrt(num2);
+        racub = Math.Cbrt(num3);
+
+        Console.WriteLine("El cuadrado del primer numero es:" + cuad + "\nLa raiz cuadrada del segundo numero es:" + raiz + "\nLa raiz cubica del tercer numero es:" + racub);
+
+    }
+}
---
+++ Dist_puntos/Dist_puntos/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Distancia entre dos puntos
+        double  x1, x2, y1, y2, dist;
+        Console.Write("Por favor digite el x1: ");
+        x1 = Double.Parse(Console.ReadLine());
+
+        Console.Write("Por favor digite el x2: ");
+        x2 = Double.Parse(Console.ReadLine());
+
+        Console.Write("Por favor digite el y1: ");
+        y1 = Double.Parse(Console.ReadLine());
+
+        Console.Write("Por favor digite el y2: ");
+        y2 = Double.Parse(Console.ReadLine());
+
+        dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+        Console.WriteLine(string.Format("la distancia entre los pares ordenados ({0},{1}) y ({2},{3}) es = {4})",x1, y1,x2,y2,dist));
+
+    }
+}

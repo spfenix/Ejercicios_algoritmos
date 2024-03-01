afe1d5fd207f71f36c044cc62421c2187003f9c8
SPFENIX <cgofenix@hotmail.com>
2024-02-29 22:43:12 -0500

    Solucion que permite determinar si un numero es par o impar.

---
+++ NumeroPar_impar/NumeroPar_impar/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    //Construir un programa que pida un número y luego diga si este número es par o no. 
+    {
+        double num;
+        Console.Write("Digita un numero: ");
+        num = Convert.ToDouble(Console.ReadLine());
+
+        if (num % 2 == 0)
+            Console.WriteLine("El numero digitado es par");
+
+        else
+            Console.WriteLine("El numero digitado es impar");
+    }
+}
+
+
+
+
+           
